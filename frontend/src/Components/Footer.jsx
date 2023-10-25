7dddee52dab03bb388cc04929c95193c54ebebf5
Jacob Kourieh <jacob.kourieh@gmail.com>
2023-10-26 00:03:49 +0200

    fix the header component and footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,13 +1,21 @@
-import './header-css.css'
+import React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 function Footer() {
     return (
-        <div className="footer">
-            <p className='footer-text'>
-                Created by
-                <a target="_blank" href="https://jacob-kourieh.com/">Jacob Kourieh</a>
-            </p>
-        </div>
+        <Container component="footer" sx={{ padding: 3, marginTop: 'auto', backgroundColor: (theme) => theme.palette.mode === 'light' ? theme.palette.grey[200] : theme.palette.grey[800] }}>
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <Typography variant="body2" color="white" align="center" sx={{ marginRight: 1 }}>
+                    Created by
+                </Typography>
+                <Link href="https://jacob-kourieh.com/" target="_blank" underline="hover" sx={{ color: '#96f58d' }}>
+                    Jacob Kourieh
+                </Link>
+            </Box>
+        </Container>
     );
 }
 
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,74 +1,83 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import "./header-css.css"
-import { FiMenu, FiX } from 'react-icons/fi';
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Box, BottomNavigation, BottomNavigationAction } from '@mui/material';
+import { Home, Image, BarChart, History, Dashboard, Audiotrack, LocalFireDepartment } from '@mui/icons-material';
+
 
 function Header() {
-    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState('home');
 
-    const handleClick = () => {
-        setOpen(!open);
-    };
+    const menuItems = [
+        { label: 'Home', icon: Home, link: '/', value: 'home' },
+        { label: 'Play', icon: LocalFireDepartment, link: '/play', value: 'play' },
+        { label: 'Music', icon: Audiotrack, link: '/randomsong', value: 'music' },
+        { label: 'Gallery', icon: Image, link: '/gallery', value: 'gallery' },
+        { label: 'Statistics', icon: BarChart, link: '/statistics', value: 'statistics' },
+        { label: 'History', icon: History, link: '/history', value: 'history' },
+        { label: 'Dashboard', icon: Dashboard, link: '/dashboard', value: 'dashboard' },
 
-    const closeMenu = () => {
-        setOpen(false);
-    };
+    ];
 
     return (
+        <AppBar position="static" sx={{ backgroundColor: 'rgb(24, 93, 98)' }}>
+            <Toolbar disableGutters>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component={Link}
+                    to="/"
+                    sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, alignItems: 'center', textDecoration: 'none', color: 'white', ml: 2 }}
+                >
+                    <div className="greeting brand">
+                        <p>MUSIC</p>
+                        <p>ARTISTS</p>
+                        <p>WAR</p>
+                    </div>
+                </Typography>
 
-        <nav className="navbar">
-            <Link to="/" className="nav-logo">
-                <div className="greeting brand">
-                    <p>MUSIC</p>
-                    <p>ARTISTS</p>
-                    <p>WAR</p>
-                </div>
-            </Link>
-            <div onClick={handleClick} className="nav-icon">
-                {open ? <FiX /> : <FiMenu />}
-            </div>
-            <ul className={open ? 'nav-links active' : 'nav-links'}>
-                <li className="nav-item">
-                    <Link to="/" className="nav-link" onClick={closeMenu}>
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/play" className="nav-link" onClick={closeMenu}>
-                        Play
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/gallery" className="nav-link" onClick={closeMenu}>
-                        Gallery
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/Statistics" className="nav-link" onClick={closeMenu}>
-                        Statistics
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/history" className="nav-link" onClick={closeMenu}>
-                        History
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/dashboard" className="nav-link" onClick={closeMenu}>
-                        Dashboard
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/randomsong" className="nav-link" onClick={closeMenu}>
-                        Music
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-
-
-
+                <Box sx={{ flexGrow: 0, width: { xs: '100%', md: 'auto' } }}>
+                    <BottomNavigation
+                        showLabels
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                        sx={{ backgroundColor: 'transparent', justifyContent: { xs: 'center', md: 'flex-start' } }}
+                    >
+                        {menuItems.map((item) => (
+                            <BottomNavigationAction
+                                key={item.value}
+                                label={item.label}
+                                icon={React.createElement(item.icon)}
+                                component={Link}
+                                to={item.link}
+                                value={item.value}
+                                sx={{
+                                    color: 'white',
+                                    '&.Mui-selected': {
+                                        color: '#96f58d',
+                                    },
+                                    '& .MuiBottomNavigationAction-label': {
+                                        fontSize: { xs: '0.7rem', md: '0.875rem' },
+                                    },
+                                    '& .MuiSvgIcon-root': {
+                                        fontSize: { xs: '1.5rem', md: '1.75rem' },
+                                    },
+                                    padding: { xs: '6px 4px', md: '6px 12px' },
+                                    minWidth: { xs: 50, md: 80 },
+                                }}
+                            />
+                        ))}
+                    </BottomNavigation>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
 export default Header;
+
+
+
+
